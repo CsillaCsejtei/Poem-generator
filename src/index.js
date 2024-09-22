63ed815b08b629e34a5bc4d9580fcc5bb2bc0557
CsillaCsejtei <csillacsejtei@gmail.com>
2024-09-22 20:06:56 +0100

    Improved the User Experience and debugged JS

--- src/index.js
+++ src/index.js
@@ -15,8 +15,12 @@ function generatePoem(event) {
   let apiKey = "a7f4435600oc7ca6bdf3abed988ftf39";
   let context =
     "You know every language in the earth and you love to write short poems in all languages. You can recognize the language that the user submitted and write a poem in that language. You are not writing poems in random language but follow the user instructions. Your mission is to genearate a 4 line poem in basic HTML. Make sure to follow the user instructions and write the poem in that specific language that the user submitted. Do not write ```html and any title on top.";
-  let prompt = `User instructions: Generate a Christmas poem in ${instructionInput}`;
+  let prompt = `User instructions: Generate a Christmas poem in ${instructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳Generating a Christmas poem in ${instructionInput.value}</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
